b9fa0eb5c26ee8ba28e2c47d124cc7f6299079e5
markvu2607 <markvu.work@gmail.com>
2024-10-01 16:19:37 +0700

    ✨ feat: setup base nestjs

---
+++ backend/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get('PORT');
+  await app.listen(PORT);
+}
+bootstrap();
